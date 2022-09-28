44b015a4ee6102d7701f79f7b1127051858d0add
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2022-09-28 16:41:46 +0100

    [New File] Added ejercicio2b.cpp

--- ejercicio2b.cpp
+++ ejercicio2b.cpp
@@ -1,9 +1,9 @@
 # include <iostream>
 using namespace std;
 
-/* ---------------------------- Exercise 2a - Practice 1 --------------------------------
+/* ---------------------------- Exercise 2b - Practice 1 --------------------------------
   Author: Sebastian Mayorquin
-  File: ejercicio2.cpp
+  File: ejercicio2b.cpp
   Date: 28/09/2022
   Grade: Software Robotics (Software Design)
 
@@ -14,7 +14,7 @@ using namespace std;
 int main(){
 
     // ----- Printing ------
-    cout << "1 2 3 4";
+    cout << "1 " << "2 " << "3 " << '4';
 
     return 0;
 }
