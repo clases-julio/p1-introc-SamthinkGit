f97de82f9a01f7921e18ec068f45798a170280e0
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2022-09-28 16:44:11 +0100

    [New File] Added ejercicio2c.cpp

---
+++ ejercicio2c.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+/* ---------------------------- Exercise 2c - Practice 1 --------------------------------
+  Author: Sebastian Mayorquin
+  File: ejercicio2c.cpp
+  Date: 28/09/2022
+  Grade: Software Robotics (Software Design)
+
+  Behaviour: Prints 4 number separated by a space each one
+  -------------------------------------------------------------------------------------
+*/
+
+int main(){
+
+    // ----- Printing ------
+    cout << "1 ";
+    cout << "2 ";
+    cout << "3 ";
+    cout << '4';
+
+    return 0;
+}
