3585bb541643e2ee04b0c2b50dc5f1bef33baa11
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2022-09-29 18:12:08 +0100

    [New File] Added ejercicio7.cpp

---
+++ ejercicio7.cpp
@@ -0,0 +1,39 @@
+# include <iostream>
+using namespace std;
+/* ---------------------------- Exercise 7 - Practice 1 --------------------------------
+  Author: Sebastian Mayorquin
+  File: ejercicio7.cpp
+  Date: 29/09/2022
+  Grade: Software Robotics (Software Design)
+
+  Behaviour: Prompts N numbers, then it prints them back separated by spaces
+ --------------------------------------------------------------------------------------
+*/
+
+int main(){
+
+    // ----- Declarations ------
+
+    int AMOUNT = 5; // Amount of numbers for prompt
+    string number;
+
+    // ----- Asking Number ------
+
+    cout << "---------- Exercise 7 - Practice 1 ----------\n";
+    cout << "Write a " << AMOUNT << "-digit number: ";
+    cin >> number;
+
+    // ----- Verifying ------
+
+    if (number.length() != 5)
+        cout << "[WARNING] " << AMOUNT << "-digit number was expected\n";
+
+    // ----- Printing Result ------
+
+    cout << "Result: ";
+
+    for (int i = 0; i < AMOUNT; i++)
+        cout << number[i] << " ";
+
+    return 0;
+}
