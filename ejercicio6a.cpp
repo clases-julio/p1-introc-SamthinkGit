3feb14bed2861b03e188f28f42087502786a54e3
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2022-09-29 15:43:32 +0100

    [New File] Added ejercicio6a.cpp and ejercicio6b.cpp

---
+++ ejercicio6a.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+using namespace std;
+
+/* ---------------------------- Exercise 6a - Practice 1 --------------------------------
+  Author: Sebastian Mayorquin
+  File: ejercicio6a.cpp
+  Date: 29/09/2022
+  Grade: Software Robotics (Software Design)
+
+  Behaviour: Prints an 8x8 chess board
+
+  --------------------------------------------------------------------------------------
+*/
+
+int main(){
+
+    // ----- Printing ------
+    cout << "* * * * * * * * \n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * * \n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * * \n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * * \n";
+    cout << " * * * * * * * *\n";
+
+}
---
+++ ejercicio6b.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+using namespace std;
+
+/* ---------------------------- Exercise 6b - Practice 1 --------------------------------
+  Author: Sebastian Mayorquin
+  File: ejercicio6b.cpp
+  Date: 29/09/2022
+  Grade: Software Robotics (Software Design)
+
+  Behaviour: Prints a NxN chess board
+
+  Notes:
+  - Constant SIZE determines the number of rows-columns
+  --------------------------------------------------------------------------------------
+*/
+
+int main() {
+
+    // ----- Declarations ------
+    int SIZE = 8;
+
+    // ----- Printing ------
+    for (int i = 0; i < SIZE/2; i++){
+
+        //Draw a row
+        for (int j = 0; j < SIZE; j++) cout << "* ";
+        cout << endl;
+
+        //Draw a row with offset
+        for (int j = 0; j < SIZE; j++) cout << " *";
+        cout << endl;
+    }
+}
